d500b7e80705f6df3c0e38438d0a322d89d97400
Cristiano Betta <cbetta@gmail.com>
2014-05-17 22:06:30 +0100

    Limiting CORS to the API

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Todos
     config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '/api/v1/*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
   end
